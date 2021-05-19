35da33d058fa09470914754c3c2123da80dc13d8
Nicholas <nicholas.mora78@yahoo.com>
2021-05-18 23:43:41 -0700

    initial commit

---
+++ tv_shows_app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from .models import Show
+
+# Create your views here.
+def index(request):
+    context ={
+        'shows': Show.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def edit(request, show_id):
+    info = Show.objects.get(id=show_id)
+    context = {
+        'show': info
+    }
+    return render(request, 'edit.html',context)
+
+def show_info(request, show_id):
+    info = Show.objects.get(id=show_id)
+    context = {
+        'show': info
+    }
+    return render(request, 'show.html', context)
+
+def create(request):
+    print(request.POST)
+    Show.objects.create(
+        title = request.POST['title'],
+        network = request.POST['network'],
+        release_date = request.POST['release_date'],
+        description = request.POST['description']
+    )
+    return redirect('/')
+
+def delete(request, show_id):
+    to_delete = Show.objects.get(id=show_id)
+    to_delete.delete()
+    return redirect('/shows')
+
+def update(request, show_id):
+    to_update = Show.objects.get(id=show_id)
+    to_update.title = request.POST['title']
+    to_update.network = request.POST['network']
+    to_update.release_date = request.POST['release_date']
+    to_update.description = request.POST['description']
+    to_update.save()
+    return redirect('/shows')
